c6f5a54c36bff3bb698aac359485c62360652aee
Peter <petrut.suciu@gmail.com>
2022-05-17 15:06:21 +0200

    Implemented giveBackAnimal

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -65,4 +65,28 @@ contract PetPark
 
         return count;
     }
+
+    function giveBackAnimal()
+    public
+    {
+        AnimalType animal = AnimalType.NONE;
+
+        bool contains_value = false;
+        for (uint i = 0; i < borrowers.length; ++i)
+        {
+            if (borrowers[i].id == msg.sender)
+            {
+                contains_value = true;
+                animal = borrowers[i].animal;
+
+                break;
+            }
+        }
+
+        require (contains_value, "No borrowed pets");
+        require (animal != AnimalType.NONE, "No borrowed pets");
+
+        // reset the type of the first "borrowed" pet
+        for (uint i = 0; i < petPark.length; ++i) { if (petPark[i] == AnimalType.BORROWED) { petPark[i] = animal; } }
+    }
 }
