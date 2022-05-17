0190d6fdcf836cf614aa24a5af589c9f7887640c
Peter <petrut.suciu@gmail.com>
2022-05-17 14:57:51 +0200

    Implement add method

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -16,11 +16,30 @@ contract PetPark
     }
 
     // -- STACK  ------------------------------
-    address private owner;
+    address      private owner;
+    AnimalType[] private petPark;
+
+    // -- EVENTS ------------------------------
+    event Added (AnimalType _type, uint _count);
 
     // -- CONSTRUCTORS ------------------------
     constructor()
     {
         owner = msg.sender;
+    }
+
+    // -- METHODS  ----------------------------
+    function add(AnimalType _type, uint _count)
+    public
+    {
+        // sanity checks
+        require (msg.sender == owner, "Not an owner");
+        require (uint(_type) > 0, "Invalid animal");
+
+        // populate pet park
+        for (uint i = 0; i != _count; ++i) { petPark.push(_type); }
+
+        // notify subscribers
+        emit Added(_type, _count);
     }
 }
