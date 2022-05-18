8efc93d6d968b151c9af21f31ab11d8e4aa9c89e
Thomas Hepner <hepner.thomas@gmail.com>
2022-05-18 11:31:01 -0700

    style: renamed variables and optimized gas utilization.

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -1,21 +1,31 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
-import "hardhat/console.sol";
+pragma solidity ^0.8.4;
+
+// Uncomment while developing code for testing.
+// import "hardhat/console.sol";
 
 contract PetPark {
     address public owner;
 
-    mapping(string => uint) public AnimalType;
-
     mapping(uint => uint) public animalCounts;
 
-    mapping(address => User) public Users;
+    mapping(address => user) public users;
 
-	mapping(string => uint8) public Gender;
+    enum animalTypes {
+        Fish,
+        Cat,
+        Dog,
+        Rabbit,
+        Parrot
+    }
 
-    struct User {
+    enum genders {
+        Male,
+        Female
+    }
+
+    struct user {
         bool hasEverBorrowed;
-        bool hasAnimal;
         uint borrowedAnimalType;
         uint gender;
         uint age;
@@ -23,23 +33,12 @@ contract PetPark {
 
     constructor() {
         owner = msg.sender;
-
-        // AnimalType
-        AnimalType["Fish"] = 1;
-        AnimalType["Cat"] = 2;
-        AnimalType["Dog"] = 3;
-        AnimalType["Rabbit"] = 4;
-        AnimalType["Parrot"] = 5;
-
-        // Gender
-        Gender["Male"] = 0;
-        Gender["Female"] = 1;
     }
 
-    modifier onlyOwner {
+    modifier onlyOwner() {
         require(msg.sender == owner, "Not an owner");
         _;
-   }
+    }
 
     event Added(uint indexed animalType, uint indexed animalCount);
 
@@ -51,30 +50,39 @@ contract PetPark {
 
     event Borrowed(uint indexed _animalType);
 
-    function borrow(uint _age, uint _gender, uint _animalType) public {
+    function borrow(
+        uint _age,
+        uint _gender,
+        uint _animalType
+    ) public {
         require(_age != 0, "Invalid Age");
         require(_animalType != 0, "Invalid animal type");
         require(animalCounts[_animalType] > 0, "Selected animal not available");
-        if(Users[msg.sender].age == 0) {
-            Users[msg.sender].age = _age;
-            Users[msg.sender].gender = _gender;
-        } 
-        require(Users[msg.sender].age == _age, "Invalid Age");
-        require(Users[msg.sender].gender == _gender, "Invalid Gender");
-        require(Users[msg.sender].hasAnimal == false, "Already adopted a pet");
+        user memory sender = users[msg.sender];
+        if (sender.age == 0) {
+            sender.age = _age;
+            sender.gender = _gender;
+            users[msg.sender] = sender;
+        }
+        require(sender.age == _age, "Invalid Age");
+        require(sender.gender == _gender, "Invalid Gender");
+        require(sender.borrowedAnimalType == 0, "Already adopted a pet");
         if (_gender == 0) {
-            require(_animalType == 3 || _animalType == 1, "Invalid animal for men");
+            require(
+                _animalType == 3 || _animalType == 1,
+                "Invalid animal for men"
+            );
         }
         if (_gender == 1 && _age < 40) {
             require(_animalType != 2, "Invalid animal for women under 40");
-        }        
-        
-        if(Users[msg.sender].hasAnimal == true) {
+        }
+
+        if (sender.borrowedAnimalType != 0) {
             giveBackAnimal();
         }
-        Users[msg.sender].hasAnimal = true;
-        Users[msg.sender].hasEverBorrowed = true;
-        Users[msg.sender].borrowedAnimalType = _animalType;
+        sender.hasEverBorrowed = true;
+        sender.borrowedAnimalType = _animalType;
+        users[msg.sender] = sender;
         animalCounts[_animalType] -= 1;
         emit Borrowed(_animalType);
     }
@@ -82,11 +90,11 @@ contract PetPark {
     event Returned(uint indexed _animalType);
 
     function giveBackAnimal() public {
-        require(Users[msg.sender].hasEverBorrowed == true, "No borrowed pets");
-        uint _animalType = Users[msg.sender].borrowedAnimalType;
+        user memory sender = users[msg.sender];
+        require(sender.hasEverBorrowed == true, "No borrowed pets");
+        uint _animalType = sender.borrowedAnimalType;
         animalCounts[_animalType] += 1;
-        Users[msg.sender].hasAnimal = false;
-        Users[msg.sender].borrowedAnimalType = 0;
+        users[msg.sender].borrowedAnimalType = 0;
         emit Returned(_animalType);
     }
 }
