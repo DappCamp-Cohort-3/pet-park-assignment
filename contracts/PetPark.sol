e4d00b37e7c452f66c38d1bdee779ce49ae4b423
vuzzer <parfaitkouess@gmail.com>
2022-05-19 12:04:00 +0000

    improvement of petpark smartcontract

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -1,32 +1,31 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-
 contract PetPark {
     address public owner;
 
-    struct Borrower{
+    struct Borrower {
         bool isBorrowed;
-        uint petType;
+        uint idPetType;
         uint gender;
         uint age;
     }
-
-    struct Pet{
+    struct Pet {
         bool isAvailable;
         uint number;
     }
-
     mapping(uint => Pet) pets;
-    mapping(address => Borrower ) listBorrower;
+    mapping(address => Borrower) borrowerAnimal;
+    Borrower person;
 
-    constructor(){
+    constructor() {
         // Set the transaction sender as the owner of the contract
         owner = msg.sender;
     }
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Not an owner");
+        person = person;
         _;
     }
 
@@ -36,61 +35,77 @@ contract PetPark {
     //Emit event when a pet is borrowed
     event Borrowed(uint petType);
 
+    function add(uint _animalType, uint _count) public onlyOwner {
+        require(_animalType > 0 && _animalType <= 5, "Invalid animal");
 
-    function add(uint _animalType, uint _count) onlyOwner public {
-        require(_animalType > 0 && _animalType <= 5 , "Invalid animal");
-
-        // Add the number of pets of the given type to the pets mapping
-        pets[_animalType] = Pet({number : _count + pets[_animalType].number, isAvailable: true });
+        // Add the number of pet of the given type to the animalCounts mapping
+        pets[_animalType] = Pet({
+            number: _count + pets[_animalType].number,
+            isAvailable: true
+        });
 
         emit Added(_animalType, pets[_animalType].number);
     }
 
-    function borrow(uint _age, uint _gender, uint _animalType)  public{
+    function borrow(
+        uint _age,
+        uint _gender,
+        uint _animalType
+    ) external {
         require(_age > 0, "Invalid Age");
 
-        require(_animalType > 0 && _animalType <= 5 , "Invalid animal type");
-        
+        require(_animalType > 0 && _animalType <= 5, "Invalid animal type");
+
         require(pets[_animalType].isAvailable, "Selected animal not available");
 
-        if(listBorrower[msg.sender].isBorrowed){
-            require(listBorrower[msg.sender].age == _age , "Invalid Age");
-            require(listBorrower[msg.sender].gender == _gender , "Invalid Gender");
+        if (person.isBorrowed) {
+            require(person.age == _age, "Invalid Age");
+            require(person.gender == _gender, "Invalid Gender");
         }
 
         //Pet is already
-        require(listBorrower[msg.sender].petType == 0, "Already adopted a pet");
+        require(person.isBorrowed != true, "Already adopted a pet");
 
-        if(_gender == 0){
-            require(_animalType == 1 || _animalType== 3,"Invalid animal for men");
-        }
-        else {
+        if (_gender == 0) {
+            require(
+                _animalType == 1 || _animalType == 3,
+                "Invalid animal for men"
+            );
+        } else {
             //Women under 40 are not allowed to borrow a Cat
-            if (_age < 40){
+            if (_age < 40) {
                 require(_animalType != 2, "Invalid animal for women under 40");
             }
         }
 
-        listBorrower[msg.sender] = Borrower(true, _animalType, _gender, _age);
+        person = Borrower(true, _animalType, _gender, _age);
 
-        pets[_animalType].number = pets[_animalType].number - 1; 
+        pets[_animalType].number = pets[_animalType].number - 1;
+
+        // isAvailabe is at false when the number of type pet is zero
+        if (pets[_animalType].number == 0) {
+            pets[_animalType].isAvailable = false;
+        }
 
         emit Borrowed(_animalType);
-
     }
 
     function giveBackAnimal() public {
-        require(listBorrower[msg.sender].isBorrowed, "No borrowed pets");
+        require(person.isBorrowed, "No borrowed pets");
 
-        //Get the type of pet borrowed to increase number
-        uint  petType = listBorrower[msg.sender].petType;    
+        //Get the id type of pet borrowed to increase number
+        uint idPetType = person.idPetType;
 
-        pets[petType].number = pets[petType].number + 1;
+        //resets the account of the person returning the animal to zero
+        person.idPetType = 0;
+        person.isBorrowed = false;
 
+        //Increase the number of animal according the id of type pet
+        pets[idPetType].number = pets[idPetType].number + 1;
+        pets[idPetType].isAvailable = true;
     }
 
-    function animalCounts(uint _animalType) view public returns (uint){
+    function animalCounts(uint _animalType) public view returns (uint) {
         return pets[_animalType].number;
     }
-
 }
