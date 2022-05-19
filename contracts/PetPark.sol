dbbfe1b08b503982eb878ed7de40f744c4c0537a
blackqueentech <deliandjam@gmail.com>
2022-05-19 01:10:34 -0700

    11/14 passing, confused on ordering of logic

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -1,18 +1,29 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-
 contract PetPark {
-
     address owner;
 
-    enum AnimalType { None, Fish, Cat, Dog, Rabbit, Parrot }
-    enum Gender { Male, Female }
+    enum AnimalType {
+        None,
+        Fish,
+        Cat,
+        Dog,
+        Rabbit,
+        Parrot
+    }
+    enum Gender {
+        None,
+        Male,
+        Female
+    }
 
-    mapping(AnimalType => int) animalCount;
-    mapping(address => AnimalType) isBorrowing;
+    mapping(AnimalType => uint) public animalCounts;
+    mapping(address => AnimalType) public isBorrowing;
+    mapping(address => uint) public userAge;
+    mapping(address => Gender) public userGender;
 
-    event Added(AnimalType animal, int count);
+    event Added(AnimalType animal, uint count);
     event Borrowed(AnimalType animal);
     event Returned(AnimalType animal);
 
@@ -40,29 +51,8 @@ contract PetPark {
         _;
     }
 
-    modifier isAvailableAnimal(AnimalType animal, int count) {
-        require(animalCount[animal] - count >= 0, "Selected animal not available");
-        _;
-    }
-
-    modifier noBoysAllowed(Gender gender, AnimalType animal) {
-        require(
-            (animal == AnimalType.Fish || animal == AnimalType.Dog) && gender == Gender.Male, 
-            "Invalid animal for men"
-        );
-        _;
-    }
-
-    modifier noYoungCatLady(uint age, Gender gender, AnimalType animal) {
-        require(
-            (animal == AnimalType.Cat && age >= 40 && gender == Gender.Female), 
-            "Invalid animal for women under 40"
-        );
-        _;
-    }
-
-    modifier notBorrowing() {
-        require(isBorrowing[msg.sender] == AnimalType.None, "Already adopted a pet");
+    modifier isAvailableAnimal(AnimalType animal) {
+        require(animalCounts[animal] > 0, "Selected animal not available");
         _;
     }
 
@@ -71,45 +61,74 @@ contract PetPark {
         _;
     }
 
-    function add(AnimalType _animal, int _count) 
-        public 
+    modifier sameAge(uint age) {
+        require(userAge[msg.sender] == age, "Invalid Age");
+        _;
+    }
+
+    modifier sameGender(Gender gender) {
+        require(userGender[msg.sender] == gender, "Invalid Gender");
+        _;
+    }
+
+    function add(AnimalType _animal, uint _count)
+        external
         onlyOwner
         isValidAnimal(_animal)
     {
-        animalCount[_animal] = animalCount[_animal] + _count;
-        emit Added(_animal, animalCount[_animal]);
+        animalCounts[_animal] = animalCounts[_animal] + _count;
+        emit Added(_animal, animalCounts[_animal]);
     }
 
-    function borrow(uint age, Gender _gender, AnimalType _animal) 
-        public
+    function borrow(
+        uint age,
+        Gender _gender,
+        AnimalType _animal
+    )
+        external
         isOldEnough(age)
         isValidAnimal2(_animal)
-        isAvailableAnimal(_animal, 1)
-        noBoysAllowed(_gender, _animal)
-        notBorrowing
+        isAvailableAnimal(_animal)
     {
-        require(
-            (_animal == AnimalType.Cat && age >= 40 && _gender == Gender.Female), 
-            "Invalid animal for women under 40"
-        );
-        animalCount[_animal] = animalCount[_animal] - 1;
+        if (_gender == Gender.Male) {
+            if (_animal != AnimalType.Fish || _animal != AnimalType.Dog) {
+                revert("Invalid animal for men");
+            }
+        }
+
+        if (_gender == Gender.Female) {
+            if (_animal == AnimalType.Cat && age < 40) {
+                revert("Invalid animal for women under 40");
+            }
+        }
+
+        if (isBorrowing[msg.sender] != AnimalType.None) {
+            revert("Already adopted a pet");
+        }
+
+        if (userAge[msg.sender] == 0) userAge[msg.sender] = age;
+        if (userGender[msg.sender] == Gender.None)
+            userGender[msg.sender] = _gender;
+
+        if (userAge[msg.sender] != age) {
+            revert("Invalid Age");
+        }
+
+        if (userGender[msg.sender] != _gender) {
+            revert("Invalid Gender");
+        }
+
         isBorrowing[msg.sender] = _animal;
+        uint count = animalCounts[_animal];
+        animalCounts[_animal] = count - 1;
         emit Borrowed(_animal);
     }
 
-    function giveBackAnimal() 
-        public
-        alreadyBorrowing
-    {
-        animalCount[isBorrowing[msg.sender]] = animalCount[isBorrowing[msg.sender]] + 1;
-        emit Returned(isBorrowing[msg.sender]);
+    function giveBackAnimal() external alreadyBorrowing {
+        AnimalType borrowedAnimal = isBorrowing[msg.sender];
+        uint count = animalCounts[borrowedAnimal];
+        animalCounts[borrowedAnimal] = count + 1;
+        emit Returned(borrowedAnimal);
         isBorrowing[msg.sender] = AnimalType.None;
-    }
-
-    function animalCounts(AnimalType animal) 
-        public
-        view
-    {
-        animalCount[animal];
     }
 }
