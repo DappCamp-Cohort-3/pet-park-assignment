8277fc98e2fb861214c019c139962a1617c1ef60
Nenne Adaora Nwodo <nenwodo@microsoft.com>
2022-05-19 22:51:39 +0100

    Address PR comments

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 contract PetPark{
 
-    // Initial animal types
     enum AnimalType {
         None, 
         Fish,
@@ -19,7 +18,7 @@ contract PetPark{
     }
 
     struct AnimalBorrower {
-        uint age;
+        uint8 age;
         Gender gender;
         AnimalType animalType;
     }
@@ -33,25 +32,32 @@ contract PetPark{
     mapping(AnimalType => uint) public animalCounts;
     mapping(address => AnimalBorrower) public animalBorrowers;
 
-    //Declare events
     event Added(AnimalType animalType, uint count);
     event Borrowed(AnimalType animalType);
+    event Returned(AnimalType animalType);
 
-    function add(AnimalType _animalType, uint _count) public {
+    modifier onlyOwner() {
         require(msg.sender == owner, "Not an owner");
-        require(_animalType > AnimalType.None && _animalType <= AnimalType.Parrot, "Invalid animal");
+        _;
+    }
 
+    modifier validAnimal(AnimalType _animalType) {
+        require(_animalType > AnimalType.None && _animalType <= AnimalType.Parrot, "Invalid animal");
+        _;
+    }
+
+    function add(AnimalType _animalType, uint _count) external onlyOwner validAnimal(_animalType){
         animalCounts[_animalType] += _count;
 
         emit Added(_animalType, _count);
     }
 
-    function borrow(uint _age, Gender _gender, AnimalType _animalType) public {
+    function borrow(uint8 _age, Gender _gender, AnimalType _animalType) public {
         require(_age > 0, "Invalid Age");
         require(_animalType > AnimalType.None && _animalType <= AnimalType.Parrot, "Invalid animal type");
         require(animalCounts[_animalType] > 0, "Selected animal not available");
 
-        AnimalBorrower storage existingAdopter = animalBorrowers[msg.sender];
+        AnimalBorrower memory existingAdopter = animalBorrowers[msg.sender];
         if (existingAdopter.age != 0) {
             require (existingAdopter.age == _age, "Invalid Age");
             require (existingAdopter.gender == _gender, "Invalid Gender");
@@ -78,5 +84,7 @@ contract PetPark{
         AnimalType animalType = animalBorrowers[msg.sender].animalType;
         animalCounts[animalType] = animalCounts[animalType] + 1;
         animalBorrowers[msg.sender].animalType = AnimalType.None;
+
+        emit Returned(animalType);
     }
 }
