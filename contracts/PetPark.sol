86fe4620bae294c77c4a5481b132cf3a6f14e082
J P <jp@Js-MacBook-Pro.attlocal.net>
2022-05-20 17:01:23 -0400

    made corrections

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -22,7 +22,6 @@ contract PetPark {
 
     struct Borrower {
         AnimalType animal;
-        bool isBorrowing;
         Gender gender;
         uint age;
     }
@@ -36,12 +35,11 @@ contract PetPark {
     event Borrowed(AnimalType _animalType);
     event Returned(AnimalType _animalType);
 
-    mapping(AnimalType => uint) animalCount;
+    mapping(AnimalType => uint) public animalCount;
     mapping(address => Borrower) public borrowers;
 
     constructor(){
         owner = msg.sender;
-
     }
 
     function add(AnimalType _animalType, uint count) public onlyOwner {
@@ -50,28 +48,24 @@ contract PetPark {
         emit Added(_animalType, count);
     }
 
-    function animalCounts(AnimalType _animalType) public view returns (uint) {
-        return animalCount[_animalType];
-    }
-
-    function borrow(uint _age, Gender _gender, AnimalType _animalType) public {
+    function borrow(uint _age, Gender _gender, AnimalType _animalType) external {
         require(_age > 0, "Invalid Age");
         require(uint(_animalType) > 0, "Invalid animal type");
         require(animalCount[_animalType] > 0, "Selected animal not available");
-        if (borrowers[msg.sender].isBorrowing == true && _age != borrowers[msg.sender].age) {
+        if (borrowers[msg.sender].animal != AnimalType.NONE && _age != borrowers[msg.sender].age) {
             revert("Invalid Age");
         }
-        if (borrowers[msg.sender].isBorrowing == true && _gender != borrowers[msg.sender].gender) {
+        if (borrowers[msg.sender].animal != AnimalType.NONE && _gender != borrowers[msg.sender].gender) {
             revert("Invalid Gender");
         }
-        require(borrowers[msg.sender].isBorrowing == false, "Already adopted a pet");
+        require(borrowers[msg.sender].animal == AnimalType.NONE, "Already adopted a pet");
         if (_gender == Gender.Male && (_animalType != AnimalType.Fish && _animalType != AnimalType.Dog)){
             revert("Invalid animal for men");
         }
         if(_gender == Gender.Female && _animalType == AnimalType.Cat && _age < 40){
             revert("Invalid animal for women under 40");
         }
-        Borrower memory currentBorrower = Borrower(_animalType, true, _gender, _age);
+        Borrower memory currentBorrower = Borrower(_animalType, _gender, _age);
         borrowers[msg.sender] = currentBorrower;
         animalCount[_animalType] = animalCount[_animalType] - 1;
         emit Borrowed(_animalType);
@@ -79,12 +73,11 @@ contract PetPark {
 
     function giveBackAnimal() public {
         Borrower memory currentBorrower = borrowers[msg.sender];
-        // require(borrowers[msg.sender].hasBorrowed == true, "No borrowed pets");
-        require(currentBorrower.isBorrowing == true, "No borrowed pets");
-        require(currentBorrower.animal != AnimalType.NONE, "No borrowed pets");
+        require(borrowers[msg.sender].animal != AnimalType.NONE, "No borrowed pets");
+        // require(currentBorrower.animal != AnimalType.NONE, "No borrowed pets");
         animalCount[currentBorrower.animal] = animalCount[currentBorrower.animal] + 1;
-        borrowers[msg.sender].isBorrowing = false;
         emit Returned(borrowers[msg.sender].animal);
+        borrowers[msg.sender].animal = AnimalType.NONE;
 
     }
 }
